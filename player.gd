49dc10937c96acaf47b218635e281e2330317e52
davor <dradic@roc-nijmegen.nl>
2023-11-01 10:00:23 +0100

    eerste commit

---
+++ player.gd
@@ -0,0 +1,46 @@
+extends CharacterBody3D
+
+@onready var head = $head
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+const MOUSE_SENS = 0.4
+const MAX_JUMPS = 2  # Maximum number of jumps allowed
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var jump_count = 0  # Counter to track the number of jumps
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * MOUSE_SENS))
+		head.rotate_x(deg_to_rad(-event.relative.y * MOUSE_SENS))
+		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	else:
+		jump_count = 0  # Reset jump counter when on the ground
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and jump_count < MAX_JUMPS:
+		velocity.y = JUMP_VELOCITY
+		jump_count += 1
+
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED * delta)
+		velocity.z = move_toward(velocity.z, 0, SPEED * delta)
+
+	# Apply the velocity to move the character.
+	move_and_slide()
